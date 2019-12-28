b48f3345537c7a1039b4a552b2af898d3e8871b6
Petrit Lame <titilame95@gmail.com>
2019-12-28 16:48:36 +0100

    fron changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,7 +26,10 @@
         <div class="auth-box bg-dark border-top border-secondary">
             <div id="loginform">
                 <div class="text-center p-t-20 p-b-20">
-                    <span class="db"><img src="{{asset('assets/images/logo.png')}}" alt="logo" /></span>
+                    <span class="db">
+{{--                        <img src="{{asset('assets/images/logo.png')}}" alt="logo" />--}}
+                        <h2>MAGAZINA</h2>
+                    </span>
                 </div>
                 <!-- Form -->
                 <form class="form-horizontal m-t-20" method="post" id="loginform" action="{{ route('login') }}">
